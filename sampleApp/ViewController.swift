6aba21341eb448ca60d239dd51b117a0ae6546c3
HarutoNishimoto <nishimoto@ei.sanken.osaka-u.ac.jp>
2019-12-18 13:42:47 +0900

    Initial Commit

---
+++ sampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleApp
+//
+//  Created by Nishimoto Haruto on H31/12/18.
+//  Copyright © 平成31年 Nishimoto Haruto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
